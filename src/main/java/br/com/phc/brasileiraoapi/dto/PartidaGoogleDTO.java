60cbaedec398401c657b7e06320dc81e0b219953
Pedro Barros <pedrosbarros@gmail.com>
2022-09-01 17:09:49 -0300

    Conectando com jsoup no google

--- src/main/java/br/com/phc/brasileiraoapi/dto/PartidaGoogleDTO.java
+++ src/main/java/br/com/phc/brasileiraoapi/dto/PartidaGoogleDTO.java
@@ -1,5 +1,37 @@
 package br.com.phc.brasileiraoapi.dto;
 
-public class PartidaGoogleDTO {
+import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+public class PartidaGoogleDTO implements Serializable {
+
+     private static final long serialVersionUID = 1L;
+     private String statusPartida;
+     private String tempoPartida;
+     
+     //Informações da equipe da casa
+     private String nomeEquipeCasa;
+     private String urlLogoEquipeCasa;
+     private String urlPlacarEquipeCasa;
+     private String golsEquipeCasa;
+     private String placarEstendidoEquipeCasa;
+     
+   //Informações da equipe Visitante
+     private String nomeEquipeVisitante;
+     private String urlLogoEquipeVisitante;
+     private String urlPlacarEquipeVisitante;
+     private String golsEquipeVisitante;
+     private String placarEstendidoEquipeVisitante;
+	  
+    
 }
--- src/main/java/br/com/phc/brasileiraoapi/util/ScrapingUtil.java
+++ src/main/java/br/com/phc/brasileiraoapi/util/ScrapingUtil.java
@@ -1,19 +1,50 @@
 package br.com.phc.brasileiraoapi.util;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import br.com.phc.brasileiraoapi.dto.PartidaGoogleDTO;
 
 public class ScrapingUtil {
-
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ScrapingUtil.class);
+	
 	private static final String BASE_URL_GOOGLE ="https://www.google.com/search?q=";
 	private static final String COMPLEMENTO_URL_GOOGLE = "&hl=pt-BR";
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String url = BASE_URL_GOOGLE + "palmeiras+x+corinthians+08/08/2020" + COMPLEMENTO_URL_GOOGLE;
 		
-		PartidaGoogleDTO partida = new PartidaGoogleDTO();
-		partida.getGolsEquipeCasa();
+		ScrapingUtil scraping = new ScrapingUtil();
+		scraping.obtemInformacoesPartida(url);
 
 	}
-
+    public PartidaGoogleDTO obtemInformacoesPartida(String url) {
+    	
+    	PartidaGoogleDTO partida = new PartidaGoogleDTO();
+    	
+    	Document document = null;
+    	
+    	try {
+			document = Jsoup.connect(url).get();
+			
+			String title = document.title();
+			LOGGER.info("Título da página: {}",title);
+			
+		} catch (IOException e) {
+			LOGGER.error("ERRO AO TENTAR CONECTAR NO GOOGLE COM JSOUP -> {}", e.getMessage());
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	
+    	
+    	return partida;
+    	
+    }
 }
